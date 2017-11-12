c0d9ecb11b5a1c41b665c704f0b48d81eed796f9
ExoerSouth <nicamx@gmail.com>
2017-11-12 16:48:40 +0300

    перед отправкой

---
+++ src/main/java/ru/javabegin/training/test_thread/TestThread_kill_1.java
@@ -0,0 +1,27 @@
+package ru.javabegin.training.test_thread;
+
+import java.util.concurrent.ExecutionException;
+
+import static ru.javabegin.training.test_thread.TestThread_12_11_2017_1.future;
+
+/**
+ * Created by Николай on 12.11.2017.
+ */
+public class TestThread_kill_1 {
+
+
+
+
+    public static void  main(String[] args)throws InterruptedException, ExecutionException {
+
+
+        System.out.println("Начинаем закрывать поток ");
+        Thread.sleep(1000);
+        //System.out.println("Начинаем закрывать поток "+ future.get());
+        System.out.println("поток работает? "+ future.isCancelled());
+            Thread.sleep(3000);
+            future.cancel(true);
+
+
+        }
+    }
