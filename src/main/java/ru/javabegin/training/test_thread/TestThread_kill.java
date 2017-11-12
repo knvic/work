6389b5871af02d7a0c15145bf9c2c390776fa4cc
ExoerSouth <nicamx@gmail.com>
2017-11-12 21:09:40 +0300

    промежуточное

--- src/main/java/ru/javabegin/training/test_thread/TestThread_kill.java
+++ src/main/java/ru/javabegin/training/test_thread/TestThread_kill.java
@@ -14,14 +14,15 @@ public class TestThread_kill {
 
     public void t_kill () throws InterruptedException, ExecutionException {
 
-
+        if(future!=null) {
         System.out.println("Начинаем закрывать поток ");
         Thread.sleep(1000);
         //System.out.println("Начинаем закрывать поток "+ future.get());
         System.out.println("поток работает? "+ future.isCancelled());
             Thread.sleep(3000);
             future.cancel(true);
-
+        }else {
+            System.out.println("не существует");}
 
         }
     }
--- src/main/java/ru/javabegin/training/test_thread/TestThread_run.java
+++ src/main/java/ru/javabegin/training/test_thread/TestThread_run.java
@@ -16,7 +16,11 @@ public static volatile  Future<String> future;
         Callable task1 = () -> {
             try {
                 for (int i=1; i<30; i++){
-                    System.out.println("работает поток "+ i);
+                    System.out.println("работает поток "+ Thread.currentThread().getName()+" "+ i);
+                    if (i==2){
+                        System.out.println("переназываем поток ");
+                        Thread.currentThread().setName("alibaba");
+                    }
                     Thread.sleep(1000);
                 }
 
