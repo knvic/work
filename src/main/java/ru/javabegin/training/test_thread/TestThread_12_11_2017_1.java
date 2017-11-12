3b6cc022b2d1596151e39e9b282c0c6c8b4373b8
ExoerSouth <nicamx@gmail.com>
2017-11-12 16:24:40 +0300

    добавлено тестирование остановки потока

---
+++ src/main/java/ru/javabegin/training/test_thread/TestThread_12_11_2017_1.java
@@ -0,0 +1,44 @@
+package ru.javabegin.training.test_thread;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by Николай on 12.11.2017.
+ */
+public class TestThread_12_11_2017_1 {
+
+public static volatile  Future<String> future;
+
+    public static void main (String[] args) throws InterruptedException, ExecutionException {
+        Callable task1 = () -> {
+            try {
+                for (int i=1; i<30; i++){
+                    System.out.println("работает поток "+ i);
+                    Thread.sleep(1000);
+                }
+
+                return "123";
+
+            }
+            catch (InterruptedException e) {
+                throw new IllegalStateException("task interrupted", e);
+            }
+        };
+
+TestThread_kill t_kill=new TestThread_kill();
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+       future = service.submit(task1);
+        //Thread.sleep(3000);
+        //future.cancel(true);
+        t_kill.t_kill();
+        service.shutdown();
+
+
+
+    }
+
+
+
+}
---
+++ src/main/java/ru/javabegin/training/test_thread/TestThread_run.java
@@ -0,0 +1,46 @@
+package ru.javabegin.training.test_thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Created by Николай on 12.11.2017.
+ */
+public class TestThread_run {
+
+public static volatile  Future<String> future;
+
+    public void t_run () throws InterruptedException {
+        Callable task1 = () -> {
+            try {
+                for (int i=1; i<30; i++){
+                    System.out.println("работает поток "+ i);
+                    Thread.sleep(1000);
+                }
+
+                return "123";
+
+            }
+            catch (InterruptedException e) {
+                throw new IllegalStateException("task interrupted", e);
+            }
+        };
+
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+        Future<String> future = service.submit(task1);
+        //Thread.sleep(3000);
+        //future.cancel(true);
+
+        service.shutdown();
+
+
+
+    }
+
+
+
+}
