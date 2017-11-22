dc9b240b81d31ff8e9ae0417a4aa1d783932adeb
nicamx <nicamx@yandex.ru>
2017-11-22 11:20:18 +0300

    commit design

---
+++ target/generated-sources/annotations/ru/javabegin/training/db/Hobby_.java
@@ -0,0 +1,16 @@
+package ru.javabegin.training.db;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Hobby.class)
+public abstract class Hobby_ {
+
+	public static volatile SingularAttribute<Hobby, String> hobbyId;
+	public static volatile SetAttribute<Hobby, Contact> contacts;
+
+}
+
